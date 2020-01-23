004aec255bdcf653fc40de8d24493f3fdb2e80de
Miguel Prada <mapra99@gmail.com>
2020-01-23 11:53:26 -0500

    created two bundle files

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import _ from 'lodash';
 import './styles.css';
+import printMe from './print.js';
 import myImage from './my_image.jpg';
 
 function component() {
   const element = document.createElement('div');
+  const btn = document.createElement('button');
 
   // Lodash
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
@@ -11,6 +13,12 @@ function component() {
   newImage.src = myImage;
   element.classList.add('hello');
   element.appendChild(newImage);
+
+  // Output management
+  btn.innerHTML = 'Click me!';
+  btn.addEventListener('click', printMe);
+
+  element.appendChild(btn);
 
   return element;
 }
